87550803691271eb5b23357676422d00a8f2d461
ahmed <bradiiahmed@isimsf.u-sfax.tn>
2025-05-22 13:12:45 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,18 @@ import tanStackRouterVite from "@tanstack/router-plugin/vite"; // Updated import
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://padre-genos-api-1.onrender.com/",
-  //       changeOrigin: true,
-  //     },
-  //     "/public": {
-  //       target: "https://padre-genos-api-1.onrender.com/",
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://padre-genos-api-1.onrender.com/",
+        changeOrigin: true,
+      },
+      "/public": {
+        target: "https://padre-genos-api-1.onrender.com/",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [tanStackRouterVite(), react()],
   test: {
     environment: "happy-dom",
