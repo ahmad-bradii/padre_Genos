d5968cf53d639cf44d22cced6224eb47aaf51f2f
ahmed <bradiiahmed@isimsf.u-sfax.tn>
2025-05-23 06:36:57 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tanStackRouterVite from "@tanstack/router-plugin/vite"; // Updated import statement
 
-const port = 3000; // change with you curent port
+// const port = 3000; // change with you curent port
 // https://vitejs.dev/config/
 export default defineConfig({
   // server: {
