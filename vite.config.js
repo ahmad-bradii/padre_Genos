400f267d715cf3ed6d3c6b4feb27caff2908218e
ahmed <bradiiahmed@isimsf.u-sfax.tn>
2025-05-23 05:53:28 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tanStackRouterVite from "@tanstack/router-plugin/vite"; // Updated import statement
 
+const port = 3000; // change with you curent port
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://padre-genos-api-1.onrender.com/",
-        changeOrigin: true,
-      },
-      "/public": {
-        target: "https://padre-genos-api-1.onrender.com/",
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: `http://localhost:${port}`,
+  //       changeOrigin: true,
+  //     },
+  //     "/public": {
+  //       target: `http://localhost:${port}`,
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   plugins: [tanStackRouterVite(), react()],
   test: {
     environment: "happy-dom",
